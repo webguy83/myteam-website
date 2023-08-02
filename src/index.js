ddc264058b698b8952e23549b6a93da4bd1500e4
Curtis <curtis_102@hotmail.com>
2023-08-02 00:24:55 -0700

    added cards

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ var hamburgerBtn = document.querySelector('.hamburger');
 var closeBtn = document.querySelector('.closeBtn');
 var menuBackdrop = document.querySelector('.menu-backdrop');
 var menuList = document.querySelector('.menu');
+var cardBtns = document.getElementsByClassName('card-btn');
+for (var _i = 0, _a = Array.from(cardBtns); _i < _a.length; _i++) {
+    var cardBtn = _a[_i];
+    cardBtn.addEventListener('click', onCardBtnClick, true);
+}
 hamburgerBtn.addEventListener('click', toggleMenu, false);
 closeBtn.addEventListener('click', toggleMenu, false);
-// menuBackdrop.addEventListener('click', toggleMenu, true);
 function toggleMenu() {
     if (menuBackdrop.classList.contains('showMenu')) {
         menuBackdrop.classList.remove('showMenu');
     }
     else {
         menuBackdrop.classList.add('showMenu');
+    }
+}
+function onCardBtnClick(e) {
+    var btn = e.target;
+    var cardHolderElm = btn.previousElementSibling;
+    var cardInnerElm = cardHolderElm.firstElementChild;
+    var imgElm = btn.firstElementChild;
+    if (btn.classList.contains('opened')) {
+        cardInnerElm.style.transform = 'none';
+        btn.classList.remove('opened');
+        imgElm.src = '../assets/icon-cross.svg';
+    }
+    else {
+        cardInnerElm.style.transform = 'rotateY(180deg)';
+        btn.classList.add('opened');
+        imgElm.src = '../assets/icon-close.svg';
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ const hamburgerBtn = document.querySelector('.hamburger')! as HTMLButtonElement;
 const closeBtn = document.querySelector('.closeBtn')! as HTMLButtonElement;
 const menuBackdrop = document.querySelector('.menu-backdrop')! as HTMLDivElement;
 const menuList = document.querySelector('.menu')! as HTMLDivElement;
+const cardBtns = document.getElementsByClassName('card-btn');
+
+for (const cardBtn of Array.from(cardBtns)) {
+  cardBtn.addEventListener('click', onCardBtnClick, true);
+}
 
 hamburgerBtn.addEventListener('click', toggleMenu, false);
 closeBtn.addEventListener('click', toggleMenu, false);
@@ -11,5 +16,22 @@ function toggleMenu() {
     menuBackdrop.classList.remove('showMenu');
   } else {
     menuBackdrop.classList.add('showMenu');
+  }
+}
+
+function onCardBtnClick(e: Event) {
+  const btn = e.target as HTMLButtonElement;
+  const cardHolderElm = btn.previousElementSibling;
+  const cardInnerElm = cardHolderElm!.firstElementChild as HTMLDivElement;
+  const imgElm = btn.firstElementChild as HTMLImageElement;
+
+  if (btn.classList.contains('opened')) {
+    cardInnerElm.style.transform = 'none';
+    btn.classList.remove('opened');
+    imgElm.src = '../assets/icon-cross.svg';
+  } else {
+    cardInnerElm.style.transform = 'rotateY(180deg)';
+    btn.classList.add('opened');
+    imgElm.src = '../assets/icon-close.svg';
   }
 }
